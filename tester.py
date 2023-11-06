46d85f9e17468672850c88710d2669dda4981736
toto99ii <omertoto99ii@gmail.com>
2023-11-06 18:09:34 +0200

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,4 +1,4 @@
-import reversiBot, basicBot, time, stableBot, betterStableBot
+import reversiBot, basicBot, time, stableBot
 bot1 = reversiBot.get_move
 bot1_color = 1
 bot1_total_time = 0
@@ -6,14 +6,13 @@ bot1_max_time = 0
 bot1_total_moves = 0
 bot1_wins = 0
 
-bot2 = stableBot.get_move
+bot2 = reversiBot.get_move
 bot2_color = 2
 bot2_total_time = 0
 bot2_max_time = 0
 bot2_total_moves = 0
 bot2_wins = 0
 
-#FIXME: doesn't works with the new optimized systems nor with numpy arrays
 starting_boards = [
 [   
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -56,6 +55,7 @@ starting_boards = [
     [0, 0, 0, 0, 0, 0, 0, 0],
 ]
 ]
+
 turns = 0
 for start in starting_boards:
     board = reversiBot.Board(start)
@@ -63,8 +63,9 @@ for start in starting_boards:
     while any(0 in l for l in board.lst):
         if len(board.get_valid_moves(bot1_color)[0]) != 0:
             t1 = time.perf_counter()
-            board.do_move(bot1_color, *bot1(bot1_color, board.lst))
+            i, j = bot1(bot1_color, board.lst)
             t2 = time.perf_counter()
+            board.optimized_do_move(bot1_color, i, j, board.get_valid_moves(bot1_color)[1][(i, j)])
             turns += 1
             bot1_total_time += t2 - t1
             bot1_max_time = max(bot1_max_time, t2 - t1)
@@ -74,8 +75,9 @@ for start in starting_boards:
         print(board)
         if len(board.get_valid_moves(bot2_color)[0]) != 0:
             t1 = time.perf_counter()
-            board.do_move(bot2_color, *bot2(bot2_color, board.lst))
+            i, j = bot2(bot2_color, board.lst)
             t2 = time.perf_counter()
+            board.optimized_do_move(bot2_color, i, j, board.get_valid_moves(bot2_color)[1][(i, j)])
             turns += 1
             bot2_total_time += t2 - t1
             bot2_max_time = max(bot2_max_time, t2 - t1)
